2e80679dda4e84268f29035318611aab9a317653
kami_lars <arslanova.25.09.00@gmail.com>
2023-06-24 01:26:27 +0600

    Composing Components up to Lesson 10

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        tags: []
-    };
+  state = {
+    value: this.props.counter.value,
+  };
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-       
-        return <ul> { this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
-    }
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
-    render() { 
-        return (
-            <div>
-                {this.state.tags.length === 0 && 'Please create a new tag!'}
-                {this.renderTags()}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary betn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state; // obj destructuring: picking count property and storing it in a separate const
+    return count === 0 ? "Zero" : count;
+    //same as return this.state.count === 0 ....
+  }
 }
- 
+
 export default Counter;
